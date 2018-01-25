1c1953b1e707b975aa900393ee3505a23e272181
Chris Reeves <chris@thisissoon.com>
2018-01-25 12:53:56 +0000

    new ServiceStore interface

--- store/service.go
+++ store/service.go
@@ -4,6 +4,10 @@ type ServicePutter interface {
 	PutService(*Service) (*Service, error)
 }
 
+type ServiceStore interface {
+	ServicePutter
+}
+
 type Service struct {
 	LogicalName string
 	Namespace   string
