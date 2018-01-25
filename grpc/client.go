a7036ca93b894b4b1b533ca5f43aa51d63c1d4ff
Chris Reeves <chris@thisissoon.com>
2018-01-25 14:02:13 +0000

    grpc client and config

---
+++ grpc/client.go
@@ -0,0 +1,18 @@
+package grpc
+
+import (
+	"google.golang.org/grpc"
+
+	needle "compass/proto/needle/v1"
+)
+
+func ClientConn(addr string) (*grpc.ClientConn, error) {
+	do := []grpc.DialOption{
+		grpc.WithInsecure(),
+	}
+	return grpc.Dial(addr, do...)
+}
+
+func NewServiceManagerClient(cc *grpc.ClientConn) needle.ServiceManagerClient {
+	return needle.NewServiceManagerClient(cc)
+}
--- grpc/config.go
+++ grpc/config.go
@@ -9,16 +9,24 @@ import (
 // Configuration keys
 var (
 	ListenAddressConfigKey = config.ConfigKey("grpc.listen_address")
+	ClientAddressKey       = config.ConfigKey("grpc.client_address")
 )
 
 func init() {
 	// Set defaults
 	viper.SetDefault(ListenAddressConfigKey.String(), ":5000")
+	viper.SetDefault(ClientAddressKey.String(), "localhosts:5000")
 	// Bind environment variables
 	viper.BindEnv(ListenAddressConfigKey.String())
+	viper.BindEnv(ClientAddressKey.String())
 }
 
 // ListenAddress returns the configured gRPC server listen address
 func ListenAddress() string {
 	return viper.GetString(ListenAddressConfigKey.String())
+}
+
+// ClientAddress returns the configured gRPC client address
+func ClientAddress() string {
+	return viper.GetString(ClientAddressKey.String())
 }
