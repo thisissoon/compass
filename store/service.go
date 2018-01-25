3d4467eaf672aec52741b1d6234e5b1a19244709
Chris Reeves <chris@thisissoon.com>
2018-01-25 15:42:18 +0000

    add create date / update time to service store model

--- store/service.go
+++ store/service.go
@@ -1,6 +1,10 @@
 package store
 
-import uuid "github.com/satori/go.uuid"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type ServicePutter interface {
 	PutService(*Service) (*Service, error)
@@ -12,6 +16,8 @@ type ServiceStore interface {
 
 type Service struct {
 	Id          uuid.UUID
+	CreateDate  time.Time
+	UpdateDate  time.Time
 	LogicalName string
 	Namespace   string
 	Description string
