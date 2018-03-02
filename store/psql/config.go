a5294e417a7b3292d91e8101f533c3b8a418039e
Chris Reeves <chris@thisissoon.com>
2018-03-02 16:16:55 +0000

    allow more granular db configuration

--- store/psql/config.go
+++ store/psql/config.go
@@ -2,26 +2,50 @@ package psql
 
 import (
 	"compass/config"
+	"fmt"
 
 	"github.com/spf13/viper"
 )
 
-// Default Data Source Name
-const defaultDsn = "postgres://postgres:postgres@localhost:5432/needle?sslmode=disable"
-
 // Configuration keys
 var (
-	DSNKey = config.ConfigKey("psql.dsn")
+	hostKey     = config.ConfigKey("psql.host")
+	dbKey       = config.ConfigKey("psql.db")
+	usernameKey = config.ConfigKey("psql.username")
+	passwordKey = config.ConfigKey("psql.password")
 )
 
 func init() {
 	// Set defaults
-	viper.SetDefault(DSNKey.String(), defaultDsn)
+	viper.SetDefault(hostKey.String(), "locahost:5432")
+	viper.SetDefault(dbKey.String(), "needle")
+	viper.SetDefault(usernameKey.String(), "postgres")
+	viper.SetDefault(passwordKey.String(), "postgres")
 	// Bind environment variables
-	viper.BindEnv(DSNKey.String())
+	viper.BindEnv(hostKey.String())
+	viper.BindEnv(dbKey.String())
+	viper.BindEnv(usernameKey.String())
+	viper.BindEnv(passwordKey.String())
+}
+
+func host() string {
+	return viper.GetString(hostKey.String())
+}
+
+func db() string {
+	return viper.GetString(dbKey.String())
+}
+
+func username() string {
+	return viper.GetString(usernameKey.String())
+}
+
+func password() string {
+	return viper.GetString(passwordKey.String())
 }
 
 // DSN returns the configured postgres dsn address
 func DSN() string {
-	return viper.GetString(DSNKey.String())
+	dsn := "postgres://%s:%s@%s/%s?sslmode=disable"
+	return fmt.Sprintf(dsn, username(), password(), host(), db())
 }
